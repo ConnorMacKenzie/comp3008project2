bc11c3ecdb8e139beecdffc861ae21533fcbe965
ConnorMacKenzie <connormack97@gmail.com>
2019-03-28 18:42:30 -0400

    add simple html page

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+var path = require('path');
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/html/index.html')));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App running at: http://localhost:${port}`))
