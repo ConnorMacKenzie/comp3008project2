518e3f56a7921c0b9cbbff92ffed0461657c4d71
ZacASTaylor <zacharystroudtaylor@cmail.carleton.ca>
2019-03-31 06:02:15 -0400

    Instructions alert and error sound

--- public/scripts/password.js
+++ public/scripts/password.js
@@ -3,19 +3,97 @@ const animal_names = ['bird', 'cat', 'cow', 'dog', 'horse', 'lion']
 
 const N = animal_names.length;
 
-// Start animation and get game info
+// Add animals pictures and buttons
 $(document).ready(function(){
-
     console.log("function: document.ready");
 
     for(let i=0; i<N; i++){
 
-        $("#td"+i).append("<img "
+        let curr_tDiv = $("#tDiv"+i);
+
+        // Animal picture
+        curr_tDiv.append("<img "
             + "src='assets/images/"
             + animal_names[i]
             + ".png'"
-            + ">")
-
+            + ">");
     }
 
-})
+    for(let i=0; i<N; i++) {
+
+        let curr_tData = $("#tData" + i)
+
+        // Short
+        curr_tData.append("<button " +
+            "class='short lengthButton'" +
+            "onclick='animalClick(\"" + animal_names[i] + "\",\"short\")'>" +
+            "Short" +
+            "</button>");
+
+        // Long
+        curr_tData.append("<button " +
+            "class='long lengthButton'" +
+            "onclick='animalClick(\"" + animal_names[i] + "\",\"long\")'>" +
+            "Long" +
+            "</button>");
+    }
+});
+
+// Add entry bar after load
+$(window).on('load', function() {
+    console.log("function: window.load");
+
+    let entry_bar_done = false
+    entry_bar_done = entry_bar()
+
+    setInterval(() => {
+        if (entry_bar_done === true){
+            customAlert("Please make a passowrd of length six by clicking long or short animal sounds.");
+        }
+    }, 100);
+});
+
+// Places entry bar
+function entry_bar() {
+    console.log("function: entry_bar");
+
+    let entry = $("#entry");
+    let table = $("#table")
+
+    let table_top_offset = table.offset().top;
+    let body_width = $("body").width();
+
+    entry.height(table_top_offset/2)
+    entry.width(body_width/3)
+    entry.offset({top:table_top_offset/4, left:body_width/3})
+
+    return true;
+}
+
+// Deals with click
+function animalClick(animal, length) {
+    console.log("function: animalClick");
+
+    let entry_text = $("#entryText")
+
+    let path = 'assets/mp3s/'
+    let audio;
+
+    if (entry_text.html().length > 5) {
+
+        audio = new Audio(path + "/error.mp3");
+    }
+
+    else {
+        entry_text.html("&#8226;" + entry_text.html())
+
+        audio = new Audio(path + animal + '_' + length + '.mp3');
+    }
+
+    audio.play();
+}
+
+// Moves entry bar on resize
+function windowResize() {
+    entry_bar()
+}
---
+++ public/scripts/util.js
@@ -0,0 +1,70 @@
+function customAlert(message, callback=null){
+    console.log("function: customAlert");
+
+    $('body').prepend(
+        "<div id='overlay' class='customAlert' " +
+        "style='" +
+            "position:fixed;" +
+            "height:100%;" +
+            "width:100%; " +
+            "background-color:rgba(0, 0, 0, 0.6);" +
+            "z-index:1;" +
+        "'></div>"
+    );
+
+    $('#overlay').append(
+        "<div id='messageBox' class='customAlert' " +
+        "style='" +
+            "background-color:white;" +
+            "z-index:2; " +
+            "position: absolute;" +
+            "top: 50%;" +
+            "left: 50%;" +
+            "transform: translate(-50%, -50%);" +
+            "border-radius:3%;" +
+            "padding-bottom: 1%;" +
+        "'></div>"
+    );
+
+    $('#messageBox').append(
+        "<h1 id='messageText' class='customAlert text'" +
+        "style='" +
+            "padding-left: 3%;" +
+            "padding-right: 3%;" +
+            "text-align: center;" +
+        "'>" +
+        message +
+        "</h1>"
+    );
+
+    $('#messageBox').append(
+        "<button id='alertButton' class='customAlert text'" +
+        "onclick='removeCustomAlert()'" +
+        "style='" +
+            "font-size: 2em;"+
+            "margin:0 auto; "+
+            "display:block; " +
+            "text-align:center; " +
+        "'>OK" +
+        "</button>"
+    );
+
+    if(callback !== null){
+        $("#alertButton").click(callback);
+    }
+
+}
+
+// Clears all elements related to custom alert
+function removeCustomAlert() {
+    let alertElements = $(".customAlert")
+
+    for (let i=0; i<alertElements.length; i++){
+        alertElements[i].remove();
+    }
+}
+
+// See if elements exist with custom alert class
+function customAlertFinished() {
+    return ($(".customAlert").length === 0)
+}
