d611c239800e0e2aea47fd469c60a753bb066595
ConnorMacKenzie <connormack97@gmail.com>
2019-04-08 20:49:10 -0400

    allow heroku to assign port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log(`App running at: http://localhost:${port}`));
+app.listen(process.env.PORT || port, () => console.log(`App running at: http://localhost:${port}`));
 
 // Middleware to display what is being requested
 app.use(express.json());
